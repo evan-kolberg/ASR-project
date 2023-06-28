471a7e87915328645ee759017235d7a6769823d6
Evan <ekprogamer13@gmail.com>
2023-06-27 21:31:48 -0400

    enhanced table

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class data(models.Model):
+    number = models.IntegerField()
+    encrypted_hash = models.CharField(max_length=256)
--- app/views.py
+++ app/views.py
@@ -1,26 +1,28 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from rsa import newkeys, encrypt
-
+from .models import data
 
 # Generate RSA key pair (outside of the function)
 public_key, private_key = newkeys(2048)
 
 
 def handle(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'table_data': data.objects.all()})
 
 
 def process_number(request):
     if request.method == 'POST':
         number = request.POST.get('number')
-        
+
         # Convert number to bytes
         number_bytes = number.encode()  # Assuming number is a string
-        
+
         # Encrypt number using RSA public key
         encrypted_number = encrypt(number_bytes, public_key)
-        
-        # Sends encrypted number as response (raw bytes)
-        return JsonResponse({'response': encrypted_number.hex()})  # Convert to hexadecimal representation if necessary
 
+        # Store the encrypted hash in the database
+        data.objects.create(number=number, encrypted_hash=encrypted_number.hex())
+
+        # Sends encrypted number as response (raw bytes)
+        return JsonResponse({'response': encrypted_number.hex()})
